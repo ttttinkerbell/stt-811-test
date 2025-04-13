e018f3a1d8dde152d849f1f186ac18428c3984d0
ttttinkerbell <ws1710523596@icloud.com>
2025-04-13 03:32:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Alzheimer's Analysis",
+    page_icon="🧠",
 )
+
+welcome = st.Page("pages/welcome.py", title="Welcome", icon="👋")
+about = st.Page("pages/about.py", title="About the Data", icon="💾")
+modeling = st.Page("pages/modeling.py", title="Modeling", icon="📊")
+documentation = st.Page("pages/documentation.py", title="Documentation", icon="📔")
+
+pg = st.navigation([welcome, about, modeling, documentation])
+pg.run()
