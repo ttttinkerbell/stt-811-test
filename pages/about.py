f89073bd09f811ca9fc8f4fc5c1b7dfdb0ac2ecf
ttttinkerbell <ws1710523596@icloud.com>
2025-04-13 06:05:30 -0400

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -18,30 +19,7 @@ from functools import wraps
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.model_selection import cross_validate
 
-def main():
-    st.title("About the Data 💾")
-  
-    st.write(
-        """ 
-            Our dataset is a collection of patient observations documenting risk factors associated with Alzheimer's disease, as well as their specific diagnosis. 
-            The data has a global perspective, coming from 20 different countries across the world, including the United States, United Kingdom, China, India, Brazil and many more, 
-            with an even spread of roughly 3700 records per country. 
-            The data was gathered and made public by a user on Kaggle. 
-            Many of the features in our dataset are frequently cited in popular scientific literature as potential links to development and progression of various dementia symptoms and Alzheimer's disease variants, 
-            making it prime for investigating relevant claims about Alzheimer's risk factors.
-        """
-    )
-
-    DATA_URL_1 = ('data/alzheimers.csv')
-    DATA_URL_2 = ('data/alzheimers_encoded.csv')
-    alzheimers = pd.read_csv(DATA_URL_1)
-    alzheimers_encoded = pd.read_csv(DATA_URL_2)
-
-    feature_names = alzheimers_encoded.columns
-    numerical_data = alzheimers_encoded.drop(columns=["Alzheimers_Diagnosis_Yes"])
-    target = alzheimers_encoded['Alzheimers_Diagnosis_Yes']
-
-    def time_it():
+def time_it():
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -53,21 +31,85 @@ def main():
         return wrapper
     return decorator
 
-    @time_it()
-    def evaluate_model(estimator, X, y, cv):
-        scoring = ["accuracy", "precision_macro", "recall_macro", "f1_macro"]
-        cv_results = cross_validate(estimator, X, y, scoring=scoring, cv=cv, n_jobs=-1)
-    
-        metrics = {
-            "accuracy": np.mean(cv_results["test_accuracy"]),
-            "precision": np.mean(cv_results["test_precision_macro"]),
-            "recall": np.mean(cv_results["test_recall_macro"]),
-            "f1": np.mean(cv_results["test_f1_macro"]),
-        }
-        return metrics
+@time_it()
+def evaluate_model(estimator, X, y, cv):
+    scoring = ["accuracy", "precision_macro", "recall_macro", "f1_macro"]
+    cv_results = cross_validate(estimator, X, y, scoring=scoring, cv=cv, n_jobs=-1)
 
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1337)
-    
+    metrics = {
+        "accuracy": np.mean(cv_results["test_accuracy"]),
+        "precision": np.mean(cv_results["test_precision_macro"]),
+        "recall": np.mean(cv_results["test_recall_macro"]),
+        "f1": np.mean(cv_results["test_f1_macro"]),
+    }
+    return metrics
+
+def get_feature_importances(model, feature_names):
+    # Tree based models usually have a feature_importances_ attribute
+    if hasattr(model, "feature_importances_"):
+        importances = model.feature_importances_
+        return list(zip(feature_names, importances))
+
+    # Linear models usually have a coef_ attribute
+    elif hasattr(model, "coef_"):
+        coef = model.coef_
+        importances = np.abs(coef[0])
+
+        return list(zip(feature_names, importances))
+    else:
+        return None
+
+def run_feature_importance_analysis(num_importances=5):
+    fitted_models = {}
+    for name, model in estimators.items():
+        print(f"Fitting {name} ...")
+        model.fit(numerical_data, target)
+        fitted_models[name] = model
+
+    for name, model in fitted_models.items():
+        importances = get_feature_importances(model, feature_names)
+        if importances is not None:
+            print(f"\n{name} feature importances:")
+            for feat, val in sorted(importances, key=lambda x: x[1], reverse=True)[:5]:
+                print(f"\t{feat}: {val:.4f}")
+        else:
+            print(f"\n{name} does not provide a direct feature importance measure.")
+
+def plot_feature_importance(model_name, num_importances = 5):
+    if model_name in estimators.keys():
+        print(f"Fitting {model_name} ...")
+        model = estimators[model_name]        
+        model.fit(numerical_data, target)
+        print("Finished fitting")
+        importances = get_feature_importances(model, numerical_data.columns)
+        sorted_importances = sorted(importances, key=lambda x: x[1], reverse=True)
+        top_importances = sorted_importances[:num_importances]
+        # Separate names and values for plotting
+        labels = [t[0] for t in top_importances]
+        values = [t[1] for t in top_importances]
+        
+        # Create a bar plot
+        plt.figure(figsize=(8, 5))
+        plt.barh(range(len(values)), values)
+        plt.yticks(range(len(values)), labels)
+        plt.title(f"Feature Importances ({model_name})")
+        plt.xlabel("importance")
+        plt.ylabel("Feature")
+        plt.show()
+
+
+def main():
+    st.title("About the Data 💾")
+
+    DATA_URL_1 = ('data/alzheimers.csv')
+    DATA_URL_2 = ('data/alzheimers_encoded.csv')
+    alzheimers = pd.read_csv(DATA_URL_1)
+    alzheimers_encoded = pd.read_csv(DATA_URL_2)
+
+    X = alzheimers_encoded.drop(columns=["Alzheimers_Diagnosis_Yes"])
+    y = alzheimers_encoded["Alzheimers_Diagnosis_Yes"]
+    feature_names = X.columns
+
     estimators = {
         "LogisticRegression": LogisticRegression(),
         "RandomForest": RandomForestClassifier(),
@@ -79,114 +121,52 @@ def main():
         "SVM": LinearSVC(C=1.0)
     }
     
-    results = {}
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1337)
+    results = {name: evaluate_model(model, X, y, skf) for name, model in estimators.items()}
 
-    for name, model in estimators.items():
-        print(f"Running {name} evaluation:")
-        metrics = evaluate_model(model, numerical_data, target, skf)
-        results[name] = metrics
-    
-    for name, metrics_dict in results.items():
-        print(f"\n{name} Metrics:")
-        for metric_name, metric_val in metrics_dict.items():
-            print(f"\t{metric_name}: {metric_val:.4f}")
-
-    def get_feature_importances(model, feature_names):
-        # Tree based models usually have a feature_importances_ attribute
-        if hasattr(model, "feature_importances_"):
-            importances = model.feature_importances_
-            return list(zip(feature_names, importances))
-    
-        # Linear models usually have a coef_ attribute
-        elif hasattr(model, "coef_"):
-            coef = model.coef_
-            importances = np.abs(coef[0])
-    
-            return list(zip(feature_names, importances))
-        else:
-            return None
-    
-    
-    def run_feature_importance_analysis(num_importances=5):
-        fitted_models = {}
-        for name, model in estimators.items():
-            print(f"Fitting {name} ...")
-            model.fit(numerical_data, target)
-            fitted_models[name] = model
-    
-        for name, model in fitted_models.items():
-            importances = get_feature_importances(model, feature_names)
-            if importances is not None:
-                print(f"\n{name} feature importances:")
-                for feat, val in sorted(importances, key=lambda x: x[1], reverse=True)[:5]:
-                    print(f"\t{feat}: {val:.4f}")
-            else:
-                print(f"\n{name} does not provide a direct feature importance measure.")
-
-    def plot_feature_importance(model_name, num_importances = 5):
-          if model_name in estimators.keys():
-              print(f"Fitting {model_name} ...")
-              model = estimators[model_name]        
-              model.fit(numerical_data, target)
-              print("Finished fitting")
-              importances = get_feature_importances(model, numerical_data.columns)
-              sorted_importances = sorted(importances, key=lambda x: x[1], reverse=True)
-              top_importances = sorted_importances[:num_importances]
-              # Separate names and values for plotting
-              labels = [t[0] for t in top_importances]
-              values = [t[1] for t in top_importances]
-              
-              # Create a bar plot
-              plt.figure(figsize=(8, 5))
-              plt.barh(range(len(values)), values)
-              plt.yticks(range(len(values)), labels)
-              plt.title(f"Feature Importances ({model_name})")
-              plt.xlabel("importance")
-              plt.ylabel("Feature")
-              plt.show()
-    
+    st.write(
+        """ 
+            Our dataset is a collection of patient observations documenting risk factors associated with Alzheimer's disease, as well as their specific diagnosis. 
+            The data has a global perspective, coming from 20 different countries across the world, including the United States, United Kingdom, China, India, Brazil and many more, 
+            with an even spread of roughly 3700 records per country. 
+            The data was gathered and made public by a user on Kaggle. 
+            Many of the features in our dataset are frequently cited in popular scientific literature as potential links to development and progression of various dementia symptoms and Alzheimer's disease variants, 
+            making it prime for investigating relevant claims about Alzheimer's risk factors.
+        """
+    )
 
     tab1, tab2, tab3, tab4 = st.tabs(["Original Data Set", "Preprocessing", "Analysis", "Feature Selection"])
 
     with tab1:
-        st.header("Original Data Set")
+        st.subheader("Original Data Set")
       
-        st.write(
-            """
-                Our datasets comes from the following two links:
-            """
-        )
+        st.markdown("Dataset source: [Kaggle](https://www.kaggle.com/datasets/ankushpanday1/alzheimers-prediction-dataset-global)")
+
+        st.write("Sample Data:")
+        st.dataframe(alzheimers)
 
         st.write(
             """
-              https://www.kaggle.com/datasets/ankushpanday1/alzheimers-prediction-dataset-global
-            """
-        )
-
-        alzheimers
-
-        st.write(
-            """
-              The data is tabular, with a mixture of 24 different qualitative and quantitative features:
+              The data is tabular, with a mixture of 24 different qualitative and quantitative features.
             """
         )
 
     with tab2:
-        st.header("Preprocessing")
+        st.subheader("Preprocessing")
       
         st.markdown(
           """
-              ### Preprocessing Steps
+              **Preprocessing Steps:**
               
-              1. **Standardization of numerical features**  
+              1. Standardization of numerical features  
                  - All continuous variables will be standardized (zero mean, unit variance).
               
-              2. **Feature-appropriate encoding methods**  
+              2. Feature-appropriate encoding methods
                  - **One-hot encoding** for **nominal variables** (no natural order).  
                  - **Ordinal encoding** for **ordered categorical variables** (with natural order).  
                  - **Label encoding** for **binary categorical features** (e.g., Yes/No for the target).
               
-              3. **Column name normalization**  
+              3. Column name normalization
                  - Convert column names to lowercase, replace spaces with underscores, and removed apostraphes.
           """
         )
@@ -203,12 +183,12 @@ def main():
             """
         )
 
-        alzheimers_encoded
+        st.dataframe(alzheimers_encoded)
     
     with tab3:
-        st.header("Analysis")
+        st.subheader("Analysis")
 
-        st.write("# Summary Statistics:")
+        st.write("**Summary Statistics:**")
         st.write("We first want to understand the structure of the dataset, including the size of the dataset, qualitative and quantitative features.")
 
         buffer = io.StringIO()
@@ -216,8 +196,6 @@ def main():
         st.text(buffer.getvalue())
 
         st.dataframe(alzheimers.describe())
-
-        alzheimers.select_dtypes(include="object").describe().style.set_caption("Categorical Columns")
 
         st.write(
             """
@@ -240,7 +218,7 @@ def main():
             """
         )
       
-        st.write("# Linear correlation analysis:")
+        st.write("**Linear correlation analysis:**")
       
         fig, ax = plt.subplots(figsize=(10, 8))
         sns.heatmap(alzheimers.select_dtypes(include='number').corr(), ax=ax, vmin=-1, vmax=1, cmap='coolwarm', annot=True, square=True)
@@ -262,7 +240,7 @@ def main():
         )
       
   with tab4:
-        st.header("Feature Selection")
+        st.subheader("Feature Selection")
 
         st.write("Analysis based on the encoded dataset:")
 
@@ -275,7 +253,7 @@ def main():
             """
         )
 
-        run_feature_importance_analysis()
+        run_feature_importance_analysis(estimators, X, y, feature_names)
 
         st.write(
             """
@@ -283,7 +261,7 @@ def main():
             """
         )
       
-        plot_feature_importance("RandomForest")
+        plot_feature_importance(estimators["RandomForest"], X, y, feature_names, "RandomForest")
         
 if __name__ == "__main__":
     main()
